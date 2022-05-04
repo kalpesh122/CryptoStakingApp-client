a317d119e72573d867d46116c194af59a302eb2e
Allen Tatenda Chinodakufa <83924454+AllenChinodakufa@users.noreply.github.com>
2022-05-04 22:17:14 +0200

    Update settings_screen.js

--- src/screens/settings_screen.js
+++ src/screens/settings_screen.js
@@ -1,14 +1,61 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/settings_screen.css';
+import fire from '../fire';
 import { styled } from '@mui/material/styles';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import BankCardComponent from '../components/bank_card/bank_card';
 import PhotoCameraOutlinedIcon from '@mui/icons-material/PhotoCameraOutlined';
 import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
-import { Container, Grid, Card, CardContent,  TextField, InputAdornment, Button, IconButton } from '@mui/material';
+import { Container, Grid, Card, CardContent, TextField, InputAdornment, Button, IconButton, Snackbar, Alert } from '@mui/material';
 
-const SettingsScreen = () => {
+const SettingsScreen = (user) => {
+    //Snackar functions
+    const [open, setOpen] = useState(false);
+    const [openError, setOpenError] = useState(false);
+
+    //Textfield functions
+    const [profileUrl, setProfileUrl] = useState('');
+    const [sponsorID, setSponsorID] = useState('');
+    const [userName, setUsername] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [sponsorName, setSponsorName] = useState('');
+
+    // Create a root reference
+    var storageRef = fire.storage().ref();
+
+    const db = fire.firestore();
+
+    db.collection("users").get().then((querySnapshot) => {
+        setEmailAddress(user.user.email);
+        querySnapshot.forEach((doc) => {
+            if (doc.data().emailAddress === user.user.email) {
+                setSponsorID(doc.data().sponsorID);
+                setEmailAddress(doc.data().emailAddress);
+                setSponsorName(doc.data().sponsorName);
+                setUsername(doc.data().userName);
+                setProfileUrl(doc.data().profileUrl);
+
+            }
+        });
+    });
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const handleCloseError = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenError(false);
+    };
+
     const Input = styled('input')({
         display: 'none',
     });
@@ -18,10 +65,26 @@ const SettingsScreen = () => {
                 <Grid container rowSpacing={1} item xs={12} md={12} lg={12} xl={12} className='card_profile'>
                     <Grid item xs={6} md={6} lg={6} xl={6}>
                         <Card className='card_avatar'>
-                            <img src='https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='Profile' />
+                            <img src={profileUrl.length === 0 ? ("https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") : profileUrl} alt='Profile' />
                         </Card>
                         <label htmlFor="icon-button-file" className='upload_button'>
-                            <Input accept="image/*" id="icon-button-file" type="file" />
+                            <Input accept="image/*" id="icon-button-file" type="file" onChange={
+                                (ev) => {
+                                    console.log(ev.target.files[0]);
+                                    storageRef.child('users/').put(ev.target.value[0].name).then((snapshot) => {
+                                        console.log('Uploaded a blob or file!');
+                                        var tangRef = storageRef.child('users/' + ev.target.value[0].name);
+                                        tangRef.getDownloadURL().then((url) => {
+                                            setProfileUrl(url);
+                                            db.doc(`users/${user.user.uid}`).update({
+                                                profileUrl: url,
+                                            }).then((docRef) => {
+                                                setOpen(true);
+                                            });
+                                        });
+                                    });
+                                }
+                            } />
                             <IconButton color="inherit" aria-label="upload picture" component="span" className='upload_icon'>
                                 <PhotoCameraOutlinedIcon />
                             </IconButton>
@@ -39,6 +102,8 @@ const SettingsScreen = () => {
                             </Grid>
                             <Grid item md={12} lg={12} xl={12} sm={12} xs={12}>
                                 <TextField fullWidth id="outlined-basic" label="Sponsor Id:" variant="outlined" className='form-control'
+                                    value={sponsorID}
+                                    onChange={(e) => setSponsorID(e.target.value)}
                                     InputProps={{
                                         startAdornment: (
                                             <InputAdornment position="start">
@@ -49,6 +114,8 @@ const SettingsScreen = () => {
                             </Grid>
                             <Grid item md={12} lg={12} xl={12} sm={12} xs={12}>
                                 <TextField fullWidth id="outlined-basic" label="username:" variant="outlined" className='form-control'
+                                    value={userName}
+                                    onChange={(e) => setUsername(e.target.value)}
                                     InputProps={{
                                         startAdornment: (
                                             <InputAdornment position="start">
@@ -58,8 +125,11 @@ const SettingsScreen = () => {
                                     }} />
                             </Grid>
                             <Grid item md={12} lg={12} xl={12} sm={12} xs={12}>
-                                <TextField fullWidth id="outlined-basic" label="E-mail ID:" variant="outlined" className='form-control'
+                                <TextField fullWidth id="filled-read-only-input" label="E-mail ID:" variant="outlined" className='form-control' aria-readonly
+                                    defaultValue={emailAddress}
+                                    value={emailAddress}
                                     InputProps={{
+                                        readOnly: true,
                                         startAdornment: (
                                             <InputAdornment position="start">
                                                 <EmailOutlinedIcon />
@@ -69,6 +139,8 @@ const SettingsScreen = () => {
                             </Grid>
                             <Grid item md={12} lg={12} xl={12} sm={12} xs={12}>
                                 <TextField fullWidth id="outlined-basic" label="Sponsor Name:" variant="outlined" className='form-control'
+                                    value={sponsorName}
+                                    onChange={(e) => setSponsorName(e.target.value)}
                                     InputProps={{
                                         startAdornment: (
                                             <InputAdornment position="start">
@@ -78,12 +150,40 @@ const SettingsScreen = () => {
                                     }} />
                             </Grid>
                             <Grid item md={12} lg={12} xl={12} sm={12} xs={12}>
-                                <Button variant="contained" className='btn'>update</Button>
+                                <Button variant="contained" className='btn' onClick={() => {
+                                    if ((emailAddress && sponsorName && sponsorID && userName) !== '') {
+                                        db.doc(`users/${user.user.uid}`).update({
+                                            emailAddress: emailAddress,
+                                            sponsorName: sponsorName,
+                                            sponsorID: sponsorID,
+                                            userName: userName,
+                                            date: new Date().getTime(),
+                                        }).then((docRef) => {
+                                            console.log("Document written with ID: ", docRef.id);
+                                            setOpen(true);
+                                        }).catch((error) => {
+                                            console.error("Error adding document: ", error);
+                                        });
+
+                                    } else {
+                                        setOpenError(true);
+                                    }
+                                }}>update</Button>
                             </Grid>
                         </CardContent>
                     </Card>
                 </Grid>
             </Grid>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    Update was successful!!
+                </Alert>
+            </Snackbar>
+            <Snackbar open={openError} autoHideDuration={6000} onClose={handleCloseError} >
+                <Alert onClose={handleCloseError} severity="warning" sx={{ width: '100%' }}>
+                    Invalid entry, please try again
+                </Alert>
+            </Snackbar >
         </Container >
     );
 }
