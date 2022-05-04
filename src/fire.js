d7f800ee137d76be06d550e8b3f30e1b92e8c6ca
Allen Tatenda Chinodakufa <83924454+AllenChinodakufa@users.noreply.github.com>
2022-05-04 19:36:26 +0200

    Create fire.js

---
+++ src/fire.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+require('firebase/compat/auth');
+require('firebase/compat/firestore');
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBHeR5DmleMctIQgCIA-5-elx5CNr5oTxI',
+    authDomain: 'crypto-stacking.firebaseapp.com',
+    projectId: 'crypto-stacking',
+    storageBucket: 'crypto-stacking.appspot.com',
+    messagingSenderId: '174272992605',
+    appId: '1:174272992605:web:bf05a04771c64a323384b4',
+    measurementId: 'G-CEVM4NGYPT'
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
